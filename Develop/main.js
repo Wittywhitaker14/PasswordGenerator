7b3e53674e71b945118168a93f950757de9054c2
Austin Whitaker <wittywhitaker14@gmail.com>
2021-10-03 16:53:18 -0600

    Finished the JS and ordering out the script to make it work

--- Develop/main.js
+++ Develop/main.js
@@ -14,10 +14,18 @@ function getSymbol(){
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+var Functions = {
+    upper: getUppercase,
+    lower: getLowercase,
+    number: getNumber,
+    symbol: getSymbol
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 generateBtn.addEventListener("click", function() {
     var length = prompt("How many characters would you like your password to be? Min:8 and Max:128")
     // console.log(length)
@@ -43,32 +51,46 @@ generateBtn.addEventListener("click", function() {
             if(symbol === 'true'){
                 getSymbol();
             } else{''};
+            
+        } else{
+            alert("Please choose a number between 8 and 128. Please click 'Generate Password' again.");
+        };
+        password = generatePassword(
+            upper, lower, number, symbol, length
 
-        console.log(+length.value, getUppercase(), getLowercase(), getNumber(), getSymbol());    
+            );
+        var passwordText = document.querySelector("#password");
 
-        var typesCount= upper + lower + number + symbol;
-        console.log("typesCount: ", typesCount);
+        passwordText.value = password;
 
-        var typesArray= [{upper}, {lower}, {number}, {symbol}].filter
-        (item => Object.values(item)[0]);
-        console.log("typesArray: ", typesArray);
-        if(typesCount === 0){
-            return '';
+        // console.log(+length.value, getUppercase(), getLowercase(), getNumber(), getSymbol());    
+        
+        function generatePassword(lower, upper, number, symbol, length){
+            
+            var generatePassword = '';
+            
+            var typesCount= upper + lower + number + symbol;
+            // console.log("typesCount: ", typesCount);
+            
+            var typesArray= [{upper}, {lower}, {number}, {symbol}].filter
+            (item => Object.values(item)[0]);
+            
+            // console.log("typesArray: ", typesArray);
+            
+            if(typesCount === 0){
+                return '';
+            }
+            
+            for(let i = 0;  i < length; i += typesCount) {
+                typesArray.forEach(type => {
+                    var funcName = Object.keys(type)[0];
+                    // console.log('funcName: ', funcName);
+                    generatePassword += Functions[funcName]();
+                });
+            }
+            var finalPassword = generatePassword;
+            
+            return finalPassword;
         }
-
-    } else{
-        alert("Please choose a number between 8 and 128. Please click 'Generate Password' again.")
-    };
-
-});
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+        
+    });
