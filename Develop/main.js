9d69430c3a17f59b462a9c164589cee596f2c85f
Austin Whitaker <wittywhitaker14@gmail.com>
2021-10-02 20:25:56 -0600

    d

--- Develop/main.js
+++ Develop/main.js
@@ -6,6 +6,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+generateBtn.addEventListener("click", function() {
+    var length = prompt("How many characters would you like your password to be? Min:8 and Max:128")
+    console.log(length);
+    
+    var uppercase= confirm("Would you like uppercase letters?")
+    console.log(uppercase);
+
+    var lowercase= confirm("Would you like lowercase letters?");
+    console.log(lowercase);
+   
+    var number= confirm("Would you like numbers?");
+    console.log(number);
+   
+    var symbol= confirm("Would you like symbols?");
+    console.log(symbol);
+
+});
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
